b58b315fa93bffc255498054c97d71c75b28a10e
Lakshmi K Anilkumar <118197815+lakshmianil2000@users.noreply.github.com>
2023-06-09 16:01:43 +0530

    code uploaded

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home, name='home'),
-    path('order',views.order, name='order'),
-     path('about',views.about, name='about'),
-]
